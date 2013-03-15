27594882d7aa7ee8144763e15d45fdeb7eb9e71d
Pablo Fernandez <heelhook@littleq.net>
2013-03-15 00:19:57 -0400

    Slight refactor of code, use extend self instead of class << self, fits better

--- lib/frisky/config.rb
+++ lib/frisky/config.rb
@@ -5,46 +5,46 @@ module Frisky
   @@config         = nil
   @@log            = nil
 
-  class << self
-    def config=(config)
-      @@config = config
+  def config=(config)
+    @@config = config
 
-      @@redis = Redis.new(host: config['redis']) if config['redis']
+    @@redis = Redis.new(host: config['redis']) if config['redis']
 
-      # Github
-      if config['github'] and config['github']['keys']
-        Frisky.config['github']['keys'].map! { |data| data.split(/:/) }
+    # Github
+    if config['github'] and config['github']['keys']
+      Frisky.config['github']['keys'].map! { |data| data.split(/:/) }
 
-        def Octokit.client_id
-          Octokit.client_id, Octokit.client_secret = Frisky.config['github']['keys'].sample
-          super
-        end
-      end
-
-      if defined? CONFIG_EXTENSIONS
-        CONFIG_EXTENSIONS.each do |method|
-          self.send(method, config)
-        end
+      def Octokit.client_id
+        Octokit.client_id, Octokit.client_secret = Frisky.config['github']['keys'].sample
+        super
       end
     end
 
-    def config_file(file)
-      Frisky.config = YAML.load_file(file)
-    end
-
-    def config; @@config; end
-    def redis; @@redis; end
-
-    def log;
-      if @@log == nil
-        @@log                 = ::Logger.new(STDOUT)
-        @@log.datetime_format = "%H:%M:%S"
-        @@log.formatter = proc do |severity, datetime, progname, msg|
-          "[#{severity}] #{datetime}: #{msg}\n"
-        end
+    if defined? CONFIG_EXTENSIONS
+      CONFIG_EXTENSIONS.each do |method|
+        self.send(method, config)
       end
-
-      @@log
     end
   end
+
+  def config_file(file)
+    Frisky.config = YAML.load_file(file)
+  end
+
+  def config; @@config; end
+  def redis; @@redis; end
+
+  def log;
+    if @@log == nil
+      @@log                 = ::Logger.new(STDOUT)
+      @@log.datetime_format = "%H:%M:%S"
+      @@log.formatter = proc do |severity, datetime, progname, msg|
+        "[#{severity}] #{datetime}: #{msg}\n"
+      end
+    end
+
+    @@log
+  end
+
+  extend self
 end
